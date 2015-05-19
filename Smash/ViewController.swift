49be56903e5ac2a3cd8dea5b07a96c0ba23b7592
ShannonArmon <shannon.armon@gmail.com>
2015-05-19 09:11:23 -0400

    app create
    Smash app

---
+++ Smash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smash
+//
+//  Created by Shannon Armon on 5/19/15.
+//  Copyright (c) 2015 Shannon Armon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
